cddc775fcc600b3f29abfa44bd9526ef1875332b
renjumr87 <renjumr87@gmail.com>
2023-08-03 01:55:31 +0530

    inital commit

---
+++ average.py
@@ -0,0 +1,6 @@
+num1 = input("Enter three numbers")
+num2 = input()
+num3 = input()
+
+average = (int(num1) + int(num2) + int(num3))/3
+print(str(average))
---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello World !!!")
+
+a = input("Enter a number")
+b = input("Enter another number")
+ans = int(a) + int(b)
+print(str(ans))
+
+
